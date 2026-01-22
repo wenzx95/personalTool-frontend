fc27d90a4af0dbfc10a944a11c08d715408c5038
自雄 <wenzixiong@cai-inc.com>
2026-01-22 23:24:28 +0800

    chore: 更新版本信息

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,9 @@
 // 自动生成的版本信息
-// 构建时间：2026-01-22 21:31:25
+// 构建时间：2026-01-22 23:08:33
 // 注意：此文件由 generate-version.sh 自动生成，请勿手动修改
 export const APP_VERSION = {
-  version: "20260122213125",
-  buildTime: "2026-01-22 21:31:25",
+  version: "20260122230833",
+  buildTime: "2026-01-22 23:08:33",
   buildDate: "2026-01-22",
-  buildTimeShort: "21:31:25"
+  buildTimeShort: "23:08:33"
 }
