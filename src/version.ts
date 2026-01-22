c784bcc7070690c12e17278ae59b77771bf7f6a0
自雄 <wenzixiong@cai-inc.com>
2026-01-22 21:35:34 +0800

    chore(frontend): 更新版本信息
    版本号: 20260122213125
    构建时间: 2026-01-22 21:31:25
    
    自动生成 by generate-version.sh

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,9 @@
 // 自动生成的版本信息
-// 构建时间：2026-01-20 00:01:09
+// 构建时间：2026-01-22 21:31:25
 // 注意：此文件由 generate-version.sh 自动生成，请勿手动修改
 export const APP_VERSION = {
-  version: "20260120000109",
-  buildTime: "2026-01-20 00:01:09",
-  buildDate: "2026-01-20",
-  buildTimeShort: "00:01:09"
+  version: "20260122213125",
+  buildTime: "2026-01-22 21:31:25",
+  buildDate: "2026-01-22",
+  buildTimeShort: "21:31:25"
 }
